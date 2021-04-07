f5af16984f962f7aadcb6975150be99738c8c547
TPlemur <TPlemur@gmail.com>
2021-04-07 13:53:17 -0700

    Empty black box

---
+++ src/main.js
@@ -0,0 +1,14 @@
+// start server at localhost:8000: "python3 -m http.server"   (in terminal)
+// in blank index. html hit ! and slect the emmit thingy to autofill
+
+console.log("hello");
+
+let config= {
+    type: Phaser.CANVAS,
+    width:640,
+    height: 480,
+    
+}
+
+let game = new Phaser.Game(config);
+
