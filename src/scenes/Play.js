34c3b4dfc502709a3723a7b1d10ba607795d8208
TPlemur <TPlemur@gmail.com>
2021-04-07 14:24:58 -0700

    upload assets & add starfield background to play scene

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,14 +4,34 @@ class Play extends Phaser.Scene {
         super("playScene")
         
     }
+
+    preload(){
+        this.load.image('starfield','assets/starfield.png')
+    }
+
+
+
     create(){
-        this.add.text(20,20,"ping");
+        //starfield background
+        this.starfield = this.add.tileSprite(
+            0,0,640,489, 'starfield').setOrigin(0,0);
+
         //green UI background element
         this.add.rectangle(
             0,borderUISize + borderPadding,
              game.config.width, borderUISize*2,
              0x00ff00
              ).setOrigin(0,0);
+            
+        //white boarders     
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
+	    this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
+    	this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
+    	this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
+
+
+
+
     }
 
 
